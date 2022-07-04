18774224b0bd9b2539c384468155f9752ada20ff
Marcello Fabrizio <marcello@pop-os.localdomain>
2022-07-04 00:39:54 -0300

    Teste com for

--- test.c
+++ test.c
@@ -24,12 +24,8 @@ int main()
 	long int bb = 5;
 	long int* bbb;
 	long int* bbbb = 10;
-	long int bbbbb[3];
 	long int bbbbbb[3] = {1, 2, 3};
-	long int* bbbbbbb[3];
 	long int* bbbbbbbb[3] = {1, 2, 3};
-	b = 25;
-	b = 2,5;
 	
 	a = a&b;
 	a = a|b;
@@ -38,42 +34,22 @@ int main()
 	a = a<<b;
 	a = a~b;
 		
-	long long int c;
-	long long int cc = 5;
-	long long int* ccc;
-	long long int* cccc = 10;
-	long long int ccccc[3];
-	long long int cccccc[3] = {1, 2, 3};
-	long long int* ccccccc[3];
-	long long int* cccccccc[3] = {1, 2, 3};
-	c = 25;
-		
 	float d;
 	float dd = 5.5;
-	float* ddd;
 	float* dddd = 10.5;
-	float ddddd[3];
-	float dddddd[3] = {1.5, 2.5, 3.5};
-	float* ddddddd[3];
 	float* dddddddd[3] = {1.5, 2.5, 3.5};
-	d = 25.5;
 		
-	double e;
 	double ee = 5.5;
-	double* eee;
 	double* eeee = 10.5;
 	double eeeee[3];
 	double eeeeee[3] = {1.5, 2.5, 3.5};
-	double* eeeeeee[3];
 	double* eeeeeeee[3] = {1.5, 2.5, 3.5};
 	e = 25.5;
-	
-	long int b;
-	long long int c;
-	float d;
-	char e;
-	double f;
-	
+
+    for (int i = 0; i < 3; i++) { 
+        e += 1;
+    }
+
 	while(result != NULL)
 	{
 		main();
